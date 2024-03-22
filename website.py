b76041483814e6e8a7f0f5f095727fcbaf12f572
kaitlyn2024 <71143346+kaitlyn2024@users.noreply.github.com>
2024-03-22 15:47:10 -0700

    Description / Comments website.py

--- website.py
+++ website.py
@@ -1,23 +1,17 @@
-#Kaitlyn
+#Kaitlyn - website server w/ video streaming
 import time
 import os
 from flask import Flask, Response, render_template, url_for
 
-#vars
-
-#functions
-def get_file_name():
-	return "static/"+time.strftime("%Y%m%d%H%M%S")+".mp4"
-
-
-
-#application
+#create application at route
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
+	#get video paths from local directory
 	files=os.listdir('./static')
-	return render_template('index.html', content=files)
+	return render_template('index.html', content=files) #use template passing file path
 
+#create server on local network
 if __name__=='__main__':
 	app.run(host="0.0.0.0", port=5000, threaded=True)
 
